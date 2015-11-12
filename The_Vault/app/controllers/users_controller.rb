66c510d817ccd6c07b44586acc5a5015ce464d60
danielle <dani.zaghian@gmail.com>
2015-11-12 14:36:32 -0800

    users can update profiles, all resources routes added

--- The_Vault/app/controllers/users_controller.rb
+++ The_Vault/app/controllers/users_controller.rb
@@ -18,4 +18,17 @@ class UsersController < ApplicationController
     redirect_to "/users/#{@user.id}" # <-- go to show
   end
 
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	user_id = params[:id]
+  	user = User.find(user_id)
+  	updated_attributes = params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city_id)
+  	user.update_attributes(updated_attributes)
+  	redirect_to user
+  end
+
+
 end
