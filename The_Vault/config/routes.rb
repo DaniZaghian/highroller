d54a485b9894411f77eae527c16ea17679d880cf
bgabot89 <bgabot89@gmail.com>
2015-11-13 11:37:36 -0800

    devbar almost 100%

--- The_Vault/config/routes.rb
+++ The_Vault/config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
 
   post "/sessions", to: "sessions#create"
 
-  delete "/sessions/:id", to: "sessions#destroy"
+
+	post "/logout", to: "sessions#destroy"
+  
+
+  #delete "/sessions/:id", to: "sessions#destroy"
 
   get "/users/:id", to: "users#show"
 
