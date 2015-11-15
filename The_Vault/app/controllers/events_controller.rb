cb72cdfea88377ded14a4416ec9f3349f2349658
danielle <dani.zaghian@gmail.com>
2015-11-15 13:33:33 -0800

    added delete method to events, individual show, user_id passed to new event form

--- The_Vault/app/controllers/events_controller.rb
+++ The_Vault/app/controllers/events_controller.rb
@@ -5,11 +5,18 @@ class EventsController < ApplicationController
   end
 
   def new
+    @user = User.find(session[:user_id])
   	@event = Event.new
   end
 
   def show
   	@event = Event.find(params[:id])
+  end
+
+  def create
+    event_params = params.require(:event).permit(:title, :body, :city_id, :event_date, :user_id)
+    @event = Event.create(event_params)
+    redirect_to city_path(@event.city_id)
   end
 
   def edit
@@ -22,6 +29,13 @@ class EventsController < ApplicationController
     updated_attributes = params.require(:event).permit(:title, :body, :city_id, :event_date)
     event.update_attributes(updated_attributes)
     redirect_to event
+  end
+
+  def destroy
+      id = params[:id]
+      event = Event.find(id)
+      event.destroy
+      redirect_to cities_path
   end
 
 
