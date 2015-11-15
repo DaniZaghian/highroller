dd6d9b26698b7fbe8f56445895dc1a7b5ce9d6bc
danielle <dani.zaghian@gmail.com>
2015-11-15 12:26:23 -0800

    now can edit events

--- The_Vault/app/controllers/events_controller.rb
+++ The_Vault/app/controllers/events_controller.rb
@@ -16,4 +16,13 @@ class EventsController < ApplicationController
   	@event = Event.find(params[:id])
   end
 
+  def update
+    event_id = params[:id]
+    event = Event.find(event_id)
+    updated_attributes = params.require(:event).permit(:title, :body, :city_id, :event_date)
+    event.update_attributes(updated_attributes)
+    redirect_to event
+  end
+
+
 end
