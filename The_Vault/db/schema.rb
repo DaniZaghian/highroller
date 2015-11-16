9bf3784f25dc592a5c18a0dd69ce2287dc4bf62a
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-11-15 22:12:05 -0800

    updated DB to work with google maps also changed seed.rb to reflect that

--- The_Vault/db/schema.rb
+++ The_Vault/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116023043) do
+ActiveRecord::Schema.define(version: 20151116054516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20151116023043) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
+    t.float    "lat"
+    t.float    "long"
   end
 
   add_index "cities", ["slug"], name: "index_cities_on_slug", unique: true, using: :btree
@@ -42,16 +44,15 @@ ActiveRecord::Schema.define(version: 20151116023043) do
     t.string   "alias"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "profile_pic_url"
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
-    t.string   "slug"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "slug"
   end
 
   add_index "users", ["slug"], name: "index_users_on_slug", unique: true, using: :btree
--- The_Vault/db/seeds.rb
+++ The_Vault/db/seeds.rb
@@ -6,45 +6,68 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'ffaker'
-SF = City.create({name: "San Francisco"})
-LD = City.create({name: "London"})
+SF = City.create({name: "San Francisco",
+                  lat: 37.775,
+                  long: -122.418})
+LD = City.create({name: "London",
+                  lat: 51.5,
+                  long: -0.116})
+MC = City.create({name: "Moscow",
+                  lat: 55.75,
+                  long: 37.61})
+TK = City.create({name: "Tokyo",
+                  lat: 35.685,
+                  long: 139.751})
+NY = City.create({name: "New York",
+                  lat: 40.714,
+                  long:-74.00})
+
 User.create!({first_name:FFaker::Name.first_name,
               last_name:FFaker::Name.last_name,
               email:FFaker::Internet.email,
               alias: "user01",
               password: 'password',
-              profile_pic_url: "http://placehold.it/150x150"})
+              avatar_file_name:"default_profile_pic.jpg"})
 
 User.create!({first_name:FFaker::Name.first_name,
               last_name:FFaker::Name.last_name,
               email:FFaker::Internet.email,
               alias: "user02",
               password: 'password',
-              profile_pic_url: "http://placehold.it/150x150"})
+              avatar_file_name:"default_profile_pic.jpg"})
 
 User.create!({first_name:FFaker::Name.first_name,
               last_name:FFaker::Name.last_name,
               email:FFaker::Internet.email,
               alias: "user03",
               password: 'password',
-              profile_pic_url: "http://placehold.it/150x150"})
+              avatar_file_name:"default_profile_pic.jpg"})
 
 User.create!({first_name:FFaker::Name.first_name,
               last_name:FFaker::Name.last_name,
               email:FFaker::Internet.email,
               alias: "user04",
               password: 'password',
-              profile_pic_url: "http://placehold.it/150x150"})
+              avatar_file_name:"default_profile_pic.jpg"})
 
 User.create!({first_name:FFaker::Name.first_name,
               last_name:FFaker::Name.last_name,
               email:FFaker::Internet.email,
               alias: "user05",
               password: 'password',
-              profile_pic_url: "http://placehold.it/150x150"})
+              avatar_file_name:"default_profile_pic.jpg"})
+
+#Standard User sql result
+# User id: 1, alias: "user01", email: "a@b.com", password_digest: "$2a$10$Obs7rnw92P0SjIDJRo7/0eWZod6WeMUr.y/0ar0NlhE...", first_name: "Riley", last_name: "Gelwicks", created_at: "2015-11-16 05:33:51", updated_at: "2015-11-16 05:33:51", avatar_file_name: "default_profile_pic.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 17165, avatar_updated_at: "2015-11-16 05:33:51", slug: "user01"
+
 
 Event.create({title: "Cockfighting" , user_id: 1, city_id: LD.id, body:"Make some money, meet new friends!", event_date: "2015-05-11 00:00:00" })
-Event.create({title: "Mandingo Fight" , user_id: 2, city_id: SF.id, body:"It's a man fight, bro!", event_date: "2015-27-11 00:00:00" })
-Event.create({title: "Thunderdome" , user_id: 3, city_id: SF.id, body:"Two men enter one man leaves", event_date: "2015-05-12 00:00:00" })
-Event.create({title: "Russian Roulette" , user_id: 4, city_id: LD.id, body:"One out of six chance of going home in a body bag", event_date: "2016-07-1 00:00:00" })
-Event.create({title: "Chemical Weapons Auction" , user_id: 5, city_id: SF.id, body:"Mustard Gas, two for a dollar!", event_date: "2015-18-11 00:00:00" })
+Event.create({title: "Mandingo Fight" , user_id: 2, city_id: SF.id, body:"It's a man fight, bro!", event_date: "2015-10-11 00:00:00" })
+Event.create({title: "Thunderdome" , user_id: 3, city_id: MC.id, body:"Two men enter one man leaves", event_date: "2015-05-12 00:00:00" })
+Event.create({title: "Russian Roulette" , user_id: 4, city_id: TK.id, body:"One out of six chance of going home in a body bag", event_date: "2016-07-01 00:00:00" })
+Event.create({title: "Chemical Weapons Auction" , user_id: 5, city_id: NY.id, body:"Mustard Gas, two for a dollar!", event_date: "2015-08-11 00:00:00" })
+Event.create({title: "Underground Street Fight" , user_id: 1, city_id: LD.id, body:"Make some money, meet new friends!", event_date: "2015-05-11 00:00:00" })
+Event.create({title: "Torture Dungeon" , user_id: 2, city_id: SF.id, body:"It's a man fight, bro!", event_date: "2015-10-11 00:00:00" })
+Event.create({title: "Llama Race" , user_id: 3, city_id: MC.id, body:"I don't know if they are alpacas or llamas but man can they run.", event_date: "2015-05-12 00:00:00" })
+Event.create({title: "Yacht Battleship" , user_id: 4, city_id: TK.id, body:"You sunk my battleship.", event_date: "2016-07-01 00:00:00" })
+Event.create({title: "Knife Fight Training - Meetup" , user_id: 5, city_id: NY.id, body:"Short swords or long?", event_date: "2015-08-11 00:00:00" })
